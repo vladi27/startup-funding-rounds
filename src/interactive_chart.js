5727478206dab959fd85afcc7f9a8a06127f833b
vladi27 <vbulvakhter@gmail.com>
2019-08-26 22:53:01 -0700

    Add drill down feature

--- src/interactive_chart.js
+++ src/interactive_chart.js
@@ -1,4 +1,5 @@
 import { SlowBuffer } from "buffer";
+import { parseSvg } from "d3-interpolate/src/transform/parse";
 
 export const interactiveChart = () => {
   let margin = { left: 80, right: 20, top: 50, bottom: 100 };
@@ -26,8 +27,15 @@ export const interactiveChart = () => {
 
   let x1 = d3.scaleBand();
 
+  let rawData;
+  let testData;
+
   let interval;
   let cleanData;
+
+  let barStep = 27;
+
+  let barPadding = 3 / barStep;
 
   //   // Y Scale
   let y = d3.scaleLinear().range([height, 0]);
@@ -50,6 +58,16 @@ export const interactiveChart = () => {
 
   x0.domain(rounds);
   x1.domain(sectors).rangeRound([0, x0.bandwidth()]);
+
+  x0.invert = function(x) {
+    var domain = x0.domain();
+    var range = x0.range();
+    var scale = d3
+      .scaleQuantize()
+      .range(domain)
+      .domain(range);
+    return scale(x);
+  };
 
   g.append("g")
     .attr("class", "y axis")
@@ -79,10 +97,31 @@ export const interactiveChart = () => {
 
   let time = 0;
 
-  d3.json("../data/funding/new_funding.json").then(function(data) {
-    //   //   //console.log(data);
+  let x3 = d3.scaleLinear().range([margin.left, width - margin.right]);
 
-    let rawData = data;
+  let xAxis2 = g =>
+    g
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0 ,${margin.top})`)
+      .call(d3.axisTop(x3).ticks(width / 90, "s"))
+      .call(g => (g.selection ? g.selection() : g).select(".domain").remove());
+
+  let yAxis2 = g =>
+    g
+      .attr("class", "y-axis")
+      .attr("transform", `translate(${margin.left + 0.5},0)`)
+      .call(g =>
+        g
+          .append("line")
+          .attr("stroke", "currentColor")
+          .attr("y1", margin.top)
+          .attr("y2", height - margin.bottom)
+      );
+
+  d3.json("../data/funding/new_funding.json").then(function(data) {
+    // console.log(data);
+
+    rawData = data;
 
     cleanData = d3
       .nest()
@@ -105,6 +144,22 @@ export const interactiveChart = () => {
         });
       })
       .entries(rawData);
+
+    testData = d3
+      .nest()
+      //     // .key(function(d) {
+      //     //   return d.funded;
+      //     // })
+      .key(function(d) {
+        return d.funded;
+      })
+      .sortKeys(d3.ascending)
+      .key(function(d) {
+        return d.sector;
+      })
+      .entries(rawData);
+
+    console.log(testData);
 
     // var rounds = cleanData.map(function(d) {
     //   return d.values
@@ -291,13 +346,22 @@ export const interactiveChart = () => {
       .attr("height", function(d) {
         return 0;
       })
-      .on("mouseover", function(d) {
-        d3.select(this).style("fill", d3.rgb(color(d.key)).darker(2));
-      })
-      .on("mouseout", function(d) {
-        d3.select(this).style("fill", color(d.key));
-      })
       .on("click", function(d) {
+        let round = x0.invert(
+          parseSvg(d3.select(this.parentNode).attr("transform")).translateX
+        );
+
+        // d3.select("g").
+        // transition(t).remove();
+        drillDown(d, slice2, round);
+      })
+      // .on("mouseover", function(d) {
+      //   d3.select(this).style("fill", d3.rgb(color(d.key)).darker(2));
+      // })
+      // .on("mouseout", function(d) {
+      //   d3.select(this).style("fill", color(d.key));
+      // })
+      .on("change", function(d) {
         if (d3.select("#play-button").text() === "Play") {
           d3.selectAll("rect")
             .transition()
@@ -346,11 +410,162 @@ export const interactiveChart = () => {
 
     $("#date-slider").slider("value", +(time + 2000));
   }
+
+  function bar(g, down, data, selector) {
+    const bars = g
+      .insert("g", selector)
+      .attr("class", "enter")
+      .attr("transform", `translate(0,${50 + barStep * barPadding})`)
+      .attr("text-anchor", "end")
+      .style("font", "10px sans-serif");
+
+    const bar = bars
+      .selectAll("g")
+      .data(data)
+      .join("g")
+      .attr("cursor", "pointer");
+    // .on("click", d => down(svg, d));
+
+    bar
+      .append("text")
+      .attr("x", 80 - 6)
+      .attr("y", (27 * (1 - 0.1)) / 2)
+      .attr("dy", ".35em")
+      .text(d => d.company);
+
+    bar
+      .append("rect")
+      .attr("x", x3(0))
+      .attr("width", function(d) {
+        console.log(x3(0));
+        return x3(d.amountRaised) - x3(0);
+      })
+      .attr("height", 27 * (1 - 0.3));
+
+    return bars;
+  }
+
+  function drillDown(d, slice, round) {
+    let unsortedData = testData[time];
+    const duration = 750;
+    const transition1 = g.transition().duration(duration);
+    const transition2 = transition1.transition();
+
+    console.log(unsortedData);
+    console.log(d);
+    console.log(round);
+
+    let newData = unsortedData.values.filter(ele => {
+      if (ele.key === d.key) {
+        return ele;
+      }
+    });
+
+    let newData2 = newData[0].values.filter(ele => {
+      if (ele.round === round) {
+        return ele;
+      }
+    });
+
+    let newData3 = newData2
+      .slice()
+      .sort((a, b) => d3.descending(a.amountRaised, b.amountRaised))
+      .slice(0, 10);
+
+    console.log(newData3);
+
+    console.log(newData2);
+
+    // let rects = g.selectAll("rect").data(newData);
+    let data = newData3;
+
+    const exit = g.selectAll("rect").attr("class", "exit");
+
+    // Entering nodes immediately obscure the clicked-on bar, so hide it.
+    exit.selectAll("rect").attr("fill-opacity", p => (p === d ? 0 : null));
+
+    // Transition exiting bars to fade out.
+    exit
+      .transition(t)
+      .attr("fill-opacity", 0)
+      .remove();
+
+    g.selectAll("g.y.axis").remove();
+
+    g.selectAll("g.x.axis").remove();
+    slice.remove();
+
+    x3.domain([0, data[0].amountRaised]);
+    console.log(x3.domain());
+
+    g.append("g").call(xAxis2);
+
+    g.append("g").call(yAxis2);
+
+    const enter = bar(g, drillDown, data, ".y-axis").attr("fill-opacity", 0);
+    console.log(enter);
+    enter.transition(transition1).attr("fill-opacity", 1);
+
+    // Transition entering bars to their new y-position.
+    enter
+      .selectAll("g")
+      .attr("transform", stack(d.index))
+      .transition(transition1)
+      .attr("transform", stagger());
+
+    // Update the x-scale domain.
+
+    // Update the x-axis.
+    g.selectAll(".x-axis")
+      .transition(transition2)
+      .call(xAxis2);
+
+    // Transition entering bars to the new x-scale.
+    enter
+      .selectAll("g")
+      .transition(transition2)
+      .attr("transform", (d, i) => `translate(0,${barStep * i})`);
+
+    // Color the bars as parents; they will fade to children if appropriate.
+    enter
+      .selectAll("rect")
+      .attr("fill", color(true))
+      .attr("fill-opacity", 1)
+      .transition(transition2)
+      // .attr("fill", d => color(!!d.children))
+      .attr("width", d => x3(d.amountRaised) - x3(0));
+  }
+
+  function stack(i) {
+    let value = 0;
+    return d => {
+      const t = `translate(${x3(value) - x3(0)},${barStep * i})`;
+      value += d.amountRaised;
+      return t;
+    };
+  }
+
+  function stagger() {
+    let value = 0;
+    return (d, i) => {
+      const t = `translate(${x3(value) - x3(0)},${barStep * i})`;
+      value += d.amountRaised;
+      return t;
+    };
+  }
+
+  // y.range([0, width]);
+
+  // x0.domain(
+  //   data.map(function(d) {
+  //     return d.company;
+  //   })
+  // );
+
+  // xAxis = d3.axisLeft(x0);
+  // yAxis = d3.axisTop(y).ticks(width / 100, "s");
 };
 
-// var menu = d3.select("#menu select").on("change", change);
-
-// function change() {
 //   clearTimeout(timeout);
 
 //   d3.transition()
